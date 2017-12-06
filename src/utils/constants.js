9b47a5699ded739a609121e76e1059ec08216a7c
jcelik <julius.cc@hotmail.com>
2017-12-06 12:48:13 +0100

    another url fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 module.exports.API_URL =
   process.env.API_URL ||
-  'https://4qttkghrrl.execute-api.eu-central-1.amazonaws.com/prod/getAnswer/'
+  'https://4qttkghrrl.execute-api.eu-central-1.amazonaws.com/prod'
