06a6e27414405c7ad2f901a58b902eddd7a092ac
jcelik <julius.cc@hotmail.com>
2017-12-06 16:03:23 +0100

    Always use production api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,3 @@
-module.exports.API_URL = process.env.API_URL || 'http://localhost:3000'
+module.exports.API_URL =
+	process.env.API_URL ||
+	'https://4qttkghrrl.execute-api.eu-central-1.amazonaws.com/prod'
